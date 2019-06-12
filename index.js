36b602d3b595d5652c01fa9d2a46a655e6bb3cc9
Anghy <anghyf27@hotmail.com>
2019-06-12 01:17:31 -0400

    Level 8 Problem 1

---
+++ index.js
@@ -0,0 +1,16 @@
+/*************************************************
+ *                   Level 8 Problem 1
+ *************************************************/
+
+//Complete the solution so that it reverses all of the words within the string passed in.
+
+function reverseWords(str) {
+  let words = str
+    .split(' ')
+    .reverse()
+    .join(' ')
+  return words
+}
+
+reverseWords('The greatest victory is that which requires no battle')
+// should return "battle no requires which that is victory greatest The"
