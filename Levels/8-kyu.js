a6950083dea7d2ae3f5bc9f90c48a0d22eb6ccc1
Anghy <anghyf27@hotmail.com>
2019-06-13 20:26:30 -0400

    Level 8 Problem 5

--- Levels/8-kyu.js
+++ Levels/8-kyu.js
@@ -47,3 +47,19 @@ function maps(arr) {
 }
 
 maps([1, 2, 3]) //[2, 4, 6]
+
+/*************************************************
+ *                   Problem 4
+ *************************************************/
+
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  if (number % 2 === 0) {
+    return 'Even'
+  } else {
+    return 'Odd'
+  }
+}
+
+even_or_odd(2) // Even
