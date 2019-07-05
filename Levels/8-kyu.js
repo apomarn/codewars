a29437f70c61c11460211d87d0ad89994ae46e0c
Anghy <anghyf27@hotmail.com>
2019-07-04 19:20:49 -0500

    problem 31

--- Levels/8-kyu.js
+++ Levels/8-kyu.js
@@ -459,3 +459,28 @@ function isDivisible(n, x, y) {
 function removeExclamationMarks(s) {
   return s.split('!').join('')
 }
+
+/*************************************************
+ *                   Problem 31
+ *************************************************/
+
+//https://www.codewars.com/kata/exes-and-ohs/train/javascript
+
+function XO(str) {
+  var x = 0
+  var o = 0
+  var word = str.toLowerCase().split('')
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === 'x') {
+      x += 1
+    } else if (word[i] === 'o') {
+      o += 1
+    }
+  }
+
+  if (x === o) {
+    return true
+  } else {
+    return false
+  }
+}
